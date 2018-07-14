d3d15d2abfc9277a4a7f02e9c79afae5f452013f
Jin Hee <jinheek89@gmail.com>
2018-07-14 10:28:47 -0400

    Adding summarization scripts

---
+++ intel/summarize.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#files=(final_xnor_pop final_xnor_pop_arch)
+#width_options=(32 64 128 256 1024 1152 1200)
+#popcount_options=(final_xnor_pop/xnor_popcount_generic final_xnor_pop/xnor_popcount_verilog final_xnor_pop_arch/xnor_popcount_arch1 final_xnor_pop_arch/xnor_popcount_arch2)
+files=(final_xnor_pop)
+width_options=(32 64 128 256)
+popcount_options=(final_xnor_pop/report_new/xnor_popcount_generic final_xnor_pop/report_new/xnor_popcount_verilog final_xnor_pop/report_new/xnor_popcount_verilog_reg)
+seeds=(1 100 1000 353234 1023830)
+
+AREA=1
+DELAY=1
+
+if [ $AREA -eq 1 ]
+then
+  for module in "${popcount_options[@]}"
+  do
+    for width in "${width_options[@]}"
+    do
+      resource="$module,$width,"
+      for seed in "${seeds[@]}"
+      do
+        dut_resource=$(grep  "; Fitter Resource Utilization by Entity" ${module}_${width}_${seed}.fit.rpt -A 10 | grep "xnor_popcount:dut|" -m 1)
+        line=$(echo $dut_resource | cut -d ' ' -f7)
+        resource+="$line,"
+      done
+      echo $resource
+    done
+  done
+fi
+
+if [ $DELAY -eq 1 ]
+then
+  for module in "${popcount_options[@]}"
+  do
+    for width in "${width_options[@]}"
+    do
+      delay="$module,$width,"
+      for seed in "${seeds[@]}"
+      do
+        delays=$(./summarize_delay.py ${module}_${width}_${seed}.sta.rpt $module)
+        delay+="$delays,"
+      done
+      echo $delay
+    done
+  done
+fi
+
---
+++ intel/summarize_delay.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+"""
+Summarizes top 10 delay paths from popcount
+adds extra component delyas as well
+
+"""
+
+from pylab import *
+import matplotlib.pyplot as plt
+import sys
+sys.path.insert(0, 'python')
+import os
+import csv
+import copy
+
+input_file = str(sys.argv[1])
+popcount_options = str(sys.argv[2])
+
+flag_new_path = -1
+path_counter = -1
+
+# Extra delays due to these particular paths
+COUT_DELAY = 0.0
+SUMOUT_DELAY = 0.0
+ADDER_DELAY = 0.0
+
+total_delay = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+total_extra_delay = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+with open(input_file) as f:
+  for line in f:
+    if "From Node" in line: # found start of that path
+      flag_new_path = 11 # skip this many lines
+      path_counter+=1
+    elif "Data Required Path" in line:
+      flag_new_path = -1
+
+    if flag_new_path > 0:
+      flag_new_path -= 1
+    elif flag_new_path == 0:
+      line_breakdown = line.split()
+      incr_delay = float(line_breakdown[3]) # 2=total, 3=incr, 4=edge, 5=type, 8=element
+      total_delay[path_counter]+=incr_delay
+      if "arch1" in popcount_options:
+        element = line_breakdown[8]
+        if "cout" in element:
+          total_extra_delay[path_counter]+=COUT_DELAY
+        elif "sumout" in element:
+          total_extra_delay[path_counter]+=SUMOUT_DELAY
+      elif "arch2" in popcount_options:
+        element = line_breakdown[8]
+        if "cout" in element:
+          total_extra_delay[path_counter]+=COUT_DELAY
+        elif "sumout" in element:
+          total_extra_delay[path_counter]+=(SUMOUT_DELAY*2+ADDER_DELAY)
+
+for i in range(10):
+  total_extra_delay[i]+=total_delay[i]
+
+#print total_delay.index(max(total_delay)), max(total_delay), total_extra_delay.index(max(total_extra_delay)), max(total_extra_delay)
+print max(total_extra_delay)
